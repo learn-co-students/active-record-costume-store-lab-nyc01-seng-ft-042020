2ecf544081fc9b14c33102b733b02f5e52e90785
blobbyblobfish <aywang6@gmail.com>
2020-04-30 18:07:28 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
         create_table :costume_stores do |t|
             t.string :name
             t.string :location
-            t.integer :costume_inventory
+            t.integer :inventory
             t.integer :employees
             t.boolean :still_in_business
             t.datetime :opening_time
