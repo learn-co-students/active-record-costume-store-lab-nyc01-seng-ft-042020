70bd4877b55cce5339065f0579b11e1b8606626f
blobbyblobfish <aywang6@gmail.com>
2020-04-30 18:15:33 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,17 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "inventory"
-    t.integer "employees"
-    t.boolean "in_business"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.integer "size"
+    t.float "price"
+    t.string "size"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.string "long_description"
+    t.text "description"
   end
 
 end
